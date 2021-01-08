09bcdc8653d3a888a454de218c2942d4a0dd3955
QiyuLiu21 <qiyuliu.rain@gmail.com>
2021-01-08 11:58:29 -0500

    .

---
+++ pythonfiles/0108Slicing.py
@@ -0,0 +1,3 @@
+family = ["Qiyu", "Di", "Derrick", "Lixin", "Wei", "Yuchong", "Tao", "Xiuxiang", "Zhao", "Xiang", "Meng", "Dan", "Lu", "Guang", "MengC", "Yi","Ping","Fan", "Mia","Qiong"]
+for i in family:
+  print("Hi" + ", " + i + "!")
